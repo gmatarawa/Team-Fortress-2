5c5a3b9e5666eb1c1024cea4e67b92e9e9a7efa3
George Cheshoare <59426320+gmatarawa@users.noreply.github.com>
2022-06-02 03:05:50 +0000

    Added fonts and text colours, as well as some text and an embedded image

--- script.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 1) {
-        $('header').addClass('visible-title');
-    }
-    else {
-        $('header').removeClass('visible-title');
-    }
-});
